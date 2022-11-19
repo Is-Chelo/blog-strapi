a5d853148234b9340ba33ec6aff1892beadaced2
Carlos Marcelo Torres Vargas <carlosmarcelotorresvargas@gmail.com>
2022-11-18 20:53:21 -0400

    issue config

--- config/database.js
+++ config/database.js
@@ -2,12 +2,18 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'mysql',
     connection: {
-      host: env('DATABASE_HOST', 'localhost'),
-      port: env.int('DATABASE_PORT', 3306),
-      database: env('DATABASE_NAME', 'blog'),
+      host: env('DATABASE_HOST', 'containers-us-west-124.railway.app'),
+      port: env.int('DATABASE_PORT', 7184),
+      database: env('DATABASE_NAME', 'railway'),
       user: env('DATABASE_USERNAME', 'root'),
-      password: env('DATABASE_PASSWORD', ''),
+      password: env('DATABASE_PASSWORD', '4hFTJmWwxKFHoFYqVgcC'),
       ssl: env.bool('DATABASE_SSL', false),
     },
   },
 });
+
+// DATABASE_HOST=containers-us-west-124.railway.app
+// DATABASE_PORT=7184
+// DATABASE_NAME=railway
+// DATABASE_USERNAME=root
+// DATABASE_PASSWORD=4hFTJmWwxKFHoFYqVgcC
